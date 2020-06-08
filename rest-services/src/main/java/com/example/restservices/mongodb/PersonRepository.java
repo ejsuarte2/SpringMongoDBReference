dba2d817eecf0f3f0423e9d059048bb63aff4094
Erick <erickduarte521@gmail.com>
2020-06-08 17:28:09 -0500

    Added comment on interface method defined

--- rest-services/src/main/java/com/example/restservices/mongodb/PersonRepository.java
+++ rest-services/src/main/java/com/example/restservices/mongodb/PersonRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends MongoRepository<Person, String> {
 
+	/** return list of people with same last name**/
   List<Person> findByLastName(@Param("name") String name);
 
 }
