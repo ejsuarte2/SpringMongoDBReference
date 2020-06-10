24bd6867fead41d59cfba3a0a8993d81891826bf
Erick <erickduarte521@gmail.com>
2020-06-09 21:19:16 -0500

    Updated CORS

--- rest-services/src/main/java/com/example/restservices/greetings/GreetingController.java
+++ rest-services/src/main/java/com/example/restservices/greetings/GreetingController.java
@@ -2,10 +2,12 @@ package com.example.restservices.greetings;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class GreetingController {
 
